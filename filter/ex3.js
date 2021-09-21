40e802006d335bdf53b09fc612bbf164a43b6c13
ChleaBourtoule <chlea.bourtoule@gmail.com>
2021-09-21 16:28:04 +0200

    exercice 3

--- filter/ex3.js
+++ filter/ex3.js
@@ -33,7 +33,16 @@ Expected OUTPUT for these sample arguments:
 */
 
 function filterOnPrice(products, maxPrice) {
+  return maxPrice.filter((price) => price <= 500);
 }
+
+console.log(filterOnPrice([
+  { name: 'iPhone X', price: 1159 },
+  { name: 'Xiaomi Mi A2', price: 209 },
+  { name: 'Samsung Galaxy Note 9', price: 992 },
+  { name: 'Huawei P20', price: 480 },
+  { name: 'Huawei P20 Pro', price: 649 }
+]));
 
 // DON'T TOUCH THIS!
 module.exports = filterOnPrice;
